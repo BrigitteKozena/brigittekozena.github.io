870fa63308a91f2aeef0b38fe32c8ed64d3c897c
Brigitte Kozena <brigittekozena@gmail.com>
2016-03-20 20:20:46 -0700

    New porfolio

---
+++ project.js
@@ -0,0 +1,17 @@
+var _banner;
+project.setAttribute("style","margin-top:"+header.offsetHeight+"px");
+
+window.onresize=function(){
+	project.setAttribute("style","margin-top:"+header.offsetHeight+"px");
+	if (_banner) BannerDiv.setAttribute("style","height:"+Banner.offsetHeight+"px");
+}
+
+Banner.onload=function(){
+	_banner=true;
+	BannerDiv.setAttribute("style","height:"+Banner.offsetHeight+"px")
+}
+
+mnu.onmousedown=function(){
+	if (menu.className=="on") menu.removeAttribute("class")
+	else menu.className="on";
+}
--- reflection.md
+++
@@ -1,6 +0,0 @@
-What did you learn about CSS padding, borders, and margin by doing this challenge?
-That they help with positioning the element on the page 
-What did you learn about CSS positioning?
-there are four type fixed, absolute, relative and static
-What aspects of your design did you find easiest to implement? What was most difficult?
-color changing an font ect was the easiest. Positioning I foudn difficult 
